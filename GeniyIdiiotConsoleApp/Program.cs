c72d328930b40436c394e5a062be1605c2fdf143
AleksejShishov <aleksejsisov43464@gmail.com>
2023-03-08 18:23:26 +0300

    Add project files.

---
+++ GeniyIdiiotConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeniyIdiiotConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] questions = new string[5];
+            questions[0] = "Сколько будет 2 плюс 2 умноженное на 2?";
+            questions[1] = "Бревно нужно разделить на 10 частей, сколько надо сделать распилов?";
+            questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
+            questions[3] = "4";
+            questions[4] = "5";
+
+            int[] annswers = new int[5];
+            annswers[0] = 6;
+            annswers[1] = 9;
+            annswers[2] = 25;
+            annswers[3] = 60;
+            annswers[4] = 2;
+        
+        }
+    }
+}
