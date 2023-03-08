5347a815cc186ef5d291d81ee22f3ee1591aafe9
AleksejShishov <aleksejsisov43464@gmail.com>
2023-03-08 20:40:33 +0300

    Программа Гений-Идиот до выполнения ДЗ

--- GeniyIdiiotConsoleApp/Program.cs
+++ GeniyIdiiotConsoleApp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GeniyIdiiotConsoleApp
 {
@@ -10,20 +6,56 @@ namespace GeniyIdiiotConsoleApp
     {
         static void Main(string[] args)
         {
-            string[] questions = new string[5];
-            questions[0] = "Сколько будет 2 плюс 2 умноженное на 2?";
+            int countQuestion = 5;
+
+            string[] questions = new string[countQuestion];
+            questions[0] = "Сколько будет 2 плюс 2, умноженное на 2?";
             questions[1] = "Бревно нужно разделить на 10 частей, сколько надо сделать распилов?";
             questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
-            questions[3] = "4";
-            questions[4] = "5";
+            questions[3] = "Укол делают каждые полчаса, сколько нужно миинут для трёх уколов?";
+            questions[4] = "Пять свечей горело до конца, но две потухли заранее. Сколько свечей осталось?";
 
-            int[] annswers = new int[5];
-            annswers[0] = 6;
-            annswers[1] = 9;
-            annswers[2] = 25;
-            annswers[3] = 60;
-            annswers[4] = 2;
-        
+            int[] answers = new int[countQuestion];
+            answers[0] = 6;
+            answers[1] = 9;
+            answers[2] = 25;
+            answers[3] = 60;
+            answers[4] = 2;
+
+            string[] diagnose  = new string[countQuestion+1];
+            diagnose[0] = "идиот";
+            diagnose[1] = "кретин";
+            diagnose[2] = "дурак";
+            diagnose[3] = "нормальный";
+            diagnose[4] = "таланнт";
+            diagnose[5] = "гений";
+
+            int countRightAnswers = 0;
+
+            Random random= new Random();
+
+
+            for (int counter=0; counter<countQuestion; counter++)
+            {
+                int randomQuestionIndex = random.Next(0,5)  ;
+                Console.WriteLine("Вопрос номер " + (counter+1));
+                Console.WriteLine(questions[randomQuestionIndex]);
+                
+                int userAnswer = Convert.ToInt32(Console.ReadLine());
+
+                int rightAnswer = answers[randomQuestionIndex];
+
+                if (userAnswer == rightAnswer)
+                { 
+                countRightAnswers++;
+                }
+
+            }
+
+            Console.WriteLine("Количество правильных решений: " + countRightAnswers);
+            Console.WriteLine("Ваш диагноз: Вы - " + diagnose[countRightAnswers]);
+           // Console.WriteLine("Ваш диагноз: Вы - " + diagnose[countRightAnswers]);
+
         }
     }
 }
