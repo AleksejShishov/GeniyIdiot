038f7f916a5163484bf6a7cecb438fb0a45e1bb1
AleksejShishov <aleksejsisov43464@gmail.com>
2023-03-08 20:56:24 +0300

    Добавил функцию, все поломалось

--- GeniyIdiiotConsoleApp/Program.cs
+++ GeniyIdiiotConsoleApp/Program.cs
@@ -4,25 +4,31 @@ namespace GeniyIdiiotConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static string[] = GetQuestions(int countQuestions)
         {
-            int countQuestion = 5;
-
-            string[] questions = new string[countQuestion];
+            string[] questions = new string[countQuestions];
             questions[0] = "Сколько будет 2 плюс 2, умноженное на 2?";
             questions[1] = "Бревно нужно разделить на 10 частей, сколько надо сделать распилов?";
             questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
             questions[3] = "Укол делают каждые полчаса, сколько нужно миинут для трёх уколов?";
             questions[4] = "Пять свечей горело до конца, но две потухли заранее. Сколько свечей осталось?";
+            return questions;
 
-            int[] answers = new int[countQuestion];
+        }
+        static void Main(string[] args)
+        {
+            int countQuestions = 5;
+            string[] questions = GetQuestions(countQuestions);
+
+
+            int[] answers = new int[countQuestions];
             answers[0] = 6;
             answers[1] = 9;
             answers[2] = 25;
             answers[3] = 60;
             answers[4] = 2;
 
-            string[] diagnose  = new string[countQuestion+1];
+            string[] diagnose  = new string[countQuestions+1];
             diagnose[0] = "идиот";
             diagnose[1] = "кретин";
             diagnose[2] = "дурак";
@@ -35,7 +41,7 @@ namespace GeniyIdiiotConsoleApp
             Random random= new Random();
 
 
-            for (int counter=0; counter<countQuestion; counter++)
+            for (int counter=0; counter<countQuestions; counter++)
             {
                 int randomQuestionIndex = random.Next(0,5)  ;
                 Console.WriteLine("Вопрос номер " + (counter+1));
